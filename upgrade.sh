f72bbd50c1d145339f0609a0f1e451b6792a6b15
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-12-22 17:52:45 +0000

    migrate

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,26 @@
 #!/bin/bash -i
 source ./vars.sh
 
+if [ -d "$WORKING_DIR/CudosData/$DATA_FOLDER-backup" ]; then
+    echo 'ERROR: Cannot upgrade because $WORKING_DIR/CudosData/$DATA_FOLDER-backup exists'
+    exit
+fi
+
+if [ -d "$WORKING_DIR/CudosNode-backup" ]; then
+    echo 'ERROR: Cannot upgrade because $WORKING_DIR/CudosData/CudosNode-backup exists'
+    exit
+fi
+
+if [ -d "$WORKING_DIR/CudosGravityBridge-backup" ]; then
+    echo 'ERROR: Cannot upgrade because $WORKING_DIR/CudosData/CudosGravityBridge-backup exists'
+    exit
+fi
+
+if [ -d "$WORKING_DIR/CudosBuilders-backup" ]; then
+    echo 'ERROR: Cannot upgrade because $WORKING_DIR/CudosData/CudosBuilders-backup exists'
+    exit
+fi
+
 echo $WORKING_DIR
 echo $NODE_NAME
 echo $START_CONTAINER_NAME
