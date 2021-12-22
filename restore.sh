a308a427e60a9775e7ae432a66d8a6ba956818ad
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-12-22 17:34:30 +0000

    syntax

--- restore.sh
+++ restore.sh
@@ -3,22 +3,22 @@ source ./vars.sh
 
 cd "$WORKING_DIR"
 
-if [ -f "./CudosData/$DATA_FOLDER-backup"]; then
+if [ -f "./CudosData/$DATA_FOLDER-backup" ]; then
     sudo rm -rf "./CudosData/$DATA_FOLDER";
     sudo mv "./CudosData/$DATA_FOLDER-backup" "./CudosData/$DATA_FOLDER";
 fi
 
-if [ -f "./CudosNode-backup"]; then
+if [ -f "./CudosNode-backup" ]; then
     sudo rm -rf "./CudosNode";
     sudo mv ./CudosNode-backup ./CudosNode;
 fi
 
-if [ -f "./CudosGravityBridge-backup"]; then
+if [ -f "./CudosGravityBridge-backup" ]; then
     sudo rm -rf "./CudosGravityBridge";
     sudo mv ./CudosGravityBridge-backup ./CudosGravityBridge;
 fi
 
-if [ -f "./CudosBuilders-backup"]; then
+if [ -f "./CudosBuilders-backup" ]; then
     sudo rm -rf "./CudosBuilders";
     sudo mv ./CudosBuilders-backup ./CudosBuilders;
 fi
